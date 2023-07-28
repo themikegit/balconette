d605270c9818870d34a7782bc2fd34d410f76d3c
m.krsmanovic <m.krsmanovic@fuentis.com>
2023-07-28 16:21:25 +0200

    stepper

--- js.js
+++ js.js
@@ -8,4 +8,26 @@ menuBtn.addEventListener('click', function(){
 })
 
 
+function goToStep(stepDiff) {
+	const steps = Array.from(document.querySelectorAll('input[name="step"]'))
+	let currentStepIndex = steps.findIndex(step => step.checked) 
+		if(currentStepIndex + stepDiff >= 0 && currentStepIndex + stepDiff < steps.length){
+			steps[currentStepIndex].checked = false;
+			steps[currentStepIndex + stepDiff].checked = true;
+
+			
+		
+			document.querySelectorAll('.stepper-button').forEach(button => button.classList.remove('disabled'));
+			document.querySelectorAll('.stepper-button')[1].innerHTML = 'Next'
+			if(currentStepIndex + stepDiff === 0){
+				document.querySelectorAll('.stepper-button')[0].classList.toggle('disabled')
+			}
+			if(currentStepIndex + stepDiff === 3){
+				document.querySelectorAll('.stepper-button')[1].innerHTML = 'Go to Cart'
+			}
+	
+		}
+  }
+
+
 
